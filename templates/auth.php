2fe34e50471069f0d7d7d86efcbeb54b657438dc
dimati9 <dimati9@mail.ru>
2020-06-06 01:43:16 +0500

    auth page

---
+++ templates/auth.php
@@ -0,0 +1,26 @@
+
+<div class="background">
+</div>
+	<div class="content auth">
+		<form action="" class="form" method="post">
+			<h3>Вход для администратора</h3>
+			<div class="inputs">
+
+				<input type="text" required placeholder="ID" class="login" name="login">
+				<div class="login-img">
+					<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path fill-rule="evenodd" clip-rule="evenodd" d="M12 23C5.92487 23 1 18.0751 1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23ZM19.3995 17.1246C20.4086 15.6703 21 13.9042 21 12C21 7.02944 16.9706 3 12 3C7.02944 3 3 7.02944 3 12C3 13.9042 3.59138 15.6703 4.6005 17.1246C5.72595 15.6381 8.3706 15 12 15C15.6294 15 18.274 15.6381 19.3995 17.1246ZM17.9647 18.7398C17.672 17.6874 15.5694 17 12 17C8.43062 17 6.328 17.6874 6.03532 18.7398C7.6233 20.1462 9.71194 21 12 21C14.2881 21 16.3767 20.1462 17.9647 18.7398ZM12 15C9.76086 15 8 13.4274 8 10C8 7.75576 9.5791 6 12 6C14.4142 6 16 7.92158 16 10.2C16 13.4796 14.2181 15 12 15ZM10 10C10 12.2693 10.8182 13 12 13C13.1777 13 14 12.2984 14 10.2C14 8.95042 13.2157 8 12 8C10.7337 8 10 8.81582 10 10Z" fill="#7B7B7B"/>
+					</svg>
+				</div>
+				<input type="password" placeholder="Пароль" class="password"  name="password">
+				<div class="password-img">
+					<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path fill-rule="evenodd" clip-rule="evenodd" d="M7 7V10H6C4.93165 10 4 10.7764 4 11.8333V20.1667C4 21.2236 4.93165 22 6 22H18C19.0684 22 20 21.2236 20 20.1667V11.8333C20 10.7764 19.0684 10 18 10H17V7C17 4.23858 14.7614 2 12 2C9.23858 2 7 4.23858 7 7ZM15 7V10H9V7C9 5.34315 10.3431 4 12 4C13.6569 4 15 5.34315 15 7ZM6 20V12H18V20H6ZM13 16C13 16.5523 12.5523 17 12 17C11.4477 17 11 16.5523 11 16C11 15.4477 11.4477 15 12 15C12.5523 15 13 15.4477 13 16Z" fill="#7B7B7B"/>
+					</svg>
+
+				</div>
+				<input type="submit" class="submit" value="Войти">
+			</div>
+		</form>
+	</div>
+
